59320388cea36556d718cdd44b0caa47fd4a2289
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-04-11 19:11:00 +0300

    Не показываем блок информации при отсутствии данных

--- src/components/trip-info.js
+++ src/components/trip-info.js
@@ -1,11 +1,19 @@
+const createInfoMarkup = (title, period) => {
+  return (
+    `    <div class="trip-info__main">
+    <h1 class="trip-info__title">${title}</h1>
+
+    <p class="trip-info__dates">${period}</p>
+  </div>
+`
+  );
+};
+
 export const createTripInfoTemplate = (title, period) => {
+  const infoMarkup = title && period ? createInfoMarkup(title, period) : ``;
   return (
     `          <section class="trip-main__trip-info  trip-info">
-    <div class="trip-info__main">
-      <h1 class="trip-info__title">${title}</h1>
-
-      <p class="trip-info__dates">${period}</p>
-    </div>
+    ${infoMarkup}
 
   </section>
 `
