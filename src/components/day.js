e0e36a9bf02b024fb5369668affe8ec71a4853c6
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-04-11 18:20:08 +0300

    Правка отображения даты дней

--- src/components/day.js
+++ src/components/day.js
@@ -1,11 +1,12 @@
 import {formatISODate} from "../utils";
 
 export const createDayTemplate = (counter, date) => {
+  const dateValue = date.toDateString().split(` `).slice(1, 3).join(` `);
   return (
     `            <li class="trip-days__item  day">
     <div class="day__info">
       <span class="day__counter">${counter}</span>
-      <time class="day__date" datetime="${formatISODate(date)}">${date.toDateString()}</time>
+      <time class="day__date" datetime="${formatISODate(date)}">${dateValue}</time>
     </div>
 
     <ul class="trip-events__list">
