d8fbb15df537c08664c197ffadfdc5d867a91c8a
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-04-24 17:40:34 +0300

    moment

--- src/components/edit-form.js
+++ src/components/edit-form.js
@@ -260,12 +260,18 @@ export class EditForm extends AbstractSmartComponent {
     this._startFlatpickr = flatpickr(startDateElement, {
       altInput: true,
       allowInput: true,
+      enableTime: true,
+      [`time_24hr`]: true,
+      altFormat: `d/m/Y H:i`,
       defaultDate: this._event.startDate || `today`,
     });
     const endDateElement = this.getElement().querySelector(`#event-end-time-1`);
     this._endFlatpickr = flatpickr(endDateElement, {
       altInput: true,
       allowInput: true,
+      enableTime: true,
+      [`time_24hr`]: true,
+      altFormat: `d/m/Y H:i`,
       defaultDate: this._event.endDate || `today`,
     });
   }
