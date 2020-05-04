adbdf203111f023e08438ad4ae929095cc011ff3
maximos-nn <52293321+maximos-nn@users.noreply.github.com>
2020-05-04 12:51:24 +0300

    Связывание компонента и контроллера фильтров

--- src/components/filter.js
+++ src/components/filter.js
@@ -3,8 +3,15 @@ import {AbstractComponent} from "./abstract-component";
 const createFilterMarkup = (filter) => {
   return (
     `<div class="trip-filters__filter">
-    <input id="filter-${filter.name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.name}" ${filter.isActive ? `checked` : ``}>
-    <label class="trip-filters__filter-label" for="filter-${filter.name}">${filter.title}</label>
+    <input
+      id="filter-${filter.name}"
+      class="trip-filters__filter-input  visually-hidden"
+      type="radio"
+      name="trip-filter"
+      value="${filter.name}"
+      ${filter.isActive ? `checked` : ``}
+      ${filter.isDisabled ? `disabled` : ``}>
+    <label class="trip-filters__filter-label" for="filter-${filter.name}">${filter.name}</label>
   </div>`
   );
 };
@@ -18,13 +25,43 @@ const createFiltersTemplate = (filters) => {
   );
 };
 
+const getActiveFilterType = (filters) => {
+  const activeFilter = filters.find((filter) => filter.isActive);
+  return activeFilter ? activeFilter.name : ``;
+};
+
 export class Filter extends AbstractComponent {
   constructor(filters) {
     super();
     this._filters = filters;
+    this._currentFilterType = getActiveFilterType(filters);
+    this._handler = null;
+
+    this._onChange = this._onChange.bind(this);
   }
 
   getTemplate() {
     return createFiltersTemplate(this._filters);
+  }
+
+  _setUIHandlers() {
+    this._element.addEventListener(`change`, this._onChange); // debounce?
+  }
+
+  _onChange() {
+    const newFilterType = this._element.querySelector(`input[name="trip-filter"]:checked`).value;
+    if (newFilterType === this._currentFilterType) {
+      return;
+    }
+    this._currentFilterType = newFilterType;
+    if (this._handler) {
+      this._handler(newFilterType);
+    }
+  }
+
+  setFilterTypeChangeHandler(handler) {
+    if (typeof handler === `function`) {
+      this._handler = handler;
+    }
   }
 }
--- src/controllers/filter-controller.js
+++ src/controllers/filter-controller.js
@@ -1,14 +1,43 @@
 import {Filter} from "../components/filter";
-import {generateFilter} from "../mocks/filter";
-import {render} from "../utils/render";
+import {render, replace} from "../utils/render";
+
+const FilterType = {
+  EVERYTHING: `everything`,
+  FUTURE: `future`,
+  PAST: `past`
+};
 
 export class FilterController {
   constructor(container, model) {
     this._container = container;
     this._model = model;
+    this._activeFilterType = FilterType.EVERYTHING;
+    this._filterComponent = null;
+
+    this._onFilterTypeChange = this._onFilterTypeChange.bind(this);
   }
 
   render() {
-    render(this._container, new Filter(generateFilter()));
+    const filters = Object.values(FilterType).map((filterType) => {
+      return {
+        name: filterType,
+        isActive: filterType === this._activeFilterType,
+        isDisabled: false
+      };
+    });
+
+    const oldFilterComponent = this._filterComponent;
+    this._filterComponent = new Filter(filters);
+    this._filterComponent.setFilterTypeChangeHandler(this._onFilterTypeChange);
+
+    if (oldFilterComponent) {
+      replace(this._filterComponent, oldFilterComponent);
+    } else {
+      render(this._container, this._filterComponent);
+    }
+  }
+
+  _onFilterTypeChange(newFilterType) {
+    this._activeFilterType = newFilterType;
   }
 }
